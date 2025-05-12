0e04121a02e56a338bc5f707c85c12382954dc5a
ABHINAV SAHU <155819722+Abhinav-2312307@users.noreply.github.com>
2025-05-12 16:15:35 +0530

    Update AppWrapper.tsx

--- components/AppWrapper.tsx
+++ components/AppWrapper.tsx
@@ -1,17 +1,15 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import Preloader from "./preloader/Preloader"
-
+import Preloader from "./preloader"
 
 export default function AppWrapper({ children }: { children: React.ReactNode }) {
   const [showContent, setShowContent] = useState(false)
 
   useEffect(() => {
-    // Match this to the duration of your preloader
     const timer = setTimeout(() => {
       setShowContent(true)
-    }, 6000) // Adjust this to match your typing + progress bar time
+    }, 6000)
 
     return () => clearTimeout(timer)
   }, [])
