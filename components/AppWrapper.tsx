7f311454d68cddea75868761ddea74e157e36eba
ABHINAV SAHU <155819722+Abhinav-2312307@users.noreply.github.com>
2025-05-12 16:29:28 +0530

    Update AppWrapper.tsx

--- components/AppWrapper.tsx
+++ components/AppWrapper.tsx
@@ -9,7 +9,7 @@ export default function AppWrapper({ children }: { children: React.ReactNode }) 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowContent(true)
-    }, 10000)
+    }, 7000)
 
     return () => clearTimeout(timer)
   }, [])
