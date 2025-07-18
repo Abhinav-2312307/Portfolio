91ad46430fd72304e2b8665d050981449ac35e07
ABHINAV SAHU <155819722+Abhinav-2312307@users.noreply.github.com>
2025-07-18 16:44:06 +0530

    Update back-to-top.tsx

--- components/back-to-top.tsx
+++ components/back-to-top.tsx
@@ -25,7 +25,7 @@ export default function BackToTop() {
 
   return (
     <button
-      className={`fixed bottom-[30px] right-[30px] w-[50px] h-[50px] bg-primary-color text-dark-color rounded-full flex items-center justify-center text-lg ${
+      className={`fixed bottom-[90px] right-[30px] w-[50px] h-[50px] bg-primary-color text-dark-color rounded-full flex items-center justify-center text-lg ${
         isMobile ? "cursor-pointer" : "cursor-none"
       } transition-all duration-300 z-[1000] shadow-md hover:bg-primary-dark hover:translate-y-[-5px] hover:shadow-lg ${
         visible ? "opacity-100 visible" : "opacity-0 invisible"
